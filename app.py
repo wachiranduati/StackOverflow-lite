484b3a26a6232a8ada4fdf6e1416a05c79658f14
wachiranduati <wachiranduati@gmail.com>
2018-12-18 09:15:28 +0300

    [Feature #162722843] created the database dbs.py

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
 	return('Welcome home bruh')
 
+@app.route('/questions', methods=['GET'])
+def questions():
+	return('This is the questions part')
+
 if __name__ == "__main__":
 	app.run(debug=True)
